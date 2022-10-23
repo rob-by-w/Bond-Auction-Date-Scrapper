7403f0396fe86781c2749b8f83095ac0c8fa0f96
rob-by-w <rwayong.code@gmail.com>
2022-10-23 17:57:07 +0900

    Enabled CORS for jgb-auction-date and ust-auction-date

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import flask
 from flask import Flask
 from json import JSONEncoder, dumps
 from datetime import date, datetime
 
 import main_jgb
 import main_ust
-
 
 app = Flask(__name__)
 
@@ -26,9 +26,17 @@ def get_jgb_auction_date():
     jgb_auction_date = main_jgb.run()
     jgb_auction_date_list = [bond.__dict__ for bond in jgb_auction_date.values()]
 
-    return dumps(jgb_auction_date_list, cls=DateTimeEncoder)
+    response = flask.jsonify(items=jgb_auction_date_list)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
 
 @app.get("/ust-auction-date")
 def get_ust_auction_date():
     ust_auction_date = main_ust.run()
-    return ust_auction_date.to_json(orient="records", date_format="iso")
+
+    response = flask.make_response(ust_auction_date.to_json(orient="records", date_format="iso"))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
